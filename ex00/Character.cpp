4d1664461ec83ca06f6f0fc6aeae7d2b8852e80d
petit_x <petit_x@epitech.eu>
2016-01-14 15:35:48 +0100

    00

--- ex00/Character.cpp
+++ ex00/Character.cpp
@@ -5,20 +5,32 @@
 ** Login   < petit_x@epitech.net >
 ** 
 ** Started on  Thu Jan 14 12:30:55 2016 petit_x - Marlon Petit
-** Last update Thu Jan 14 12:47:06 2016 Marlon Petit
+** Last update Thu Jan 14 15:29:49 2016 Marlon Petit
 */
 
-Character::Character(const std::string &name, int level)
+#include "Character.hh"
+
+Character::Character(const std::string &name, int level):_name(name), _level(level)
 {
-  this->_name = *name;
   this->_level = level;
   this->_pv = 100;
   this->_power = 100;
+  this->_strength = 5;
+  this->_stamina = 5;
+  this->_intelligence = 5;
+  this->_spirit = 5;
+  this->_agility = 5;
+  this->Range = CLOSE;
+  std::cout << getName() << " Created" << std::endl;
+}
+
+Character::~Character()
+{
 }
 
 const std::string	&Character::getName() const
 {
-  return (&this->_name);
+  return (this->_name);
 }
 
 int	Character::getLvl() const
@@ -38,5 +50,47 @@ int	Character::getPower() const
 
 int	Character::CloseAttack()
 {
-  
+  if (getPower() >= 10)
+    {
+      this->_power -= 10;
+      std::cout << getName() << " strikes with a wood stick" << std::endl;
+      return (10 + this->_strength);
+    }
+  std::cout << getName() << " out of power" << std::endl;
+  return (0);
+}
+
+int	Character::RangeAttack()
+{
+  if (getPower() >= 10)
+    {
+      this->_power -= 10;
+      std::cout << getName() << " launches a stone" << std::endl;
+      return (5 + this->_strength);
+    }
+  std::cout << getName() << " out of power" << std::endl;
+  return (0);
+}
+
+void	Character::Heal()
+{
+  this->_pv += 50;
+  if (this->_pv > 100)
+    this->_pv = 100;
+  std::cout << getName() << " takes a potion" << std::endl;
+}
+
+void	Character::RestorePower()
+{
+  this->_power = 100;
+  std::cout << getName() << " eats" << std::endl;
+}
+
+void	Character::TakeDamage(int _damage)
+{
+  this->_pv -= _damage;
+  if (getPv() > 0)
+    std::cout << getName() << " takes " << _damage << " damage" << std::endl;
+  else
+    std::cout << getName() << " out of combat" << std::endl;
 }
--- ex00/Character.hh
+++ ex00/Character.hh
@@ -5,21 +5,31 @@
 ** Login   < petit_x@epitech.net >
 ** 
 ** Started on  Thu Jan 14 12:31:02 2016 petit_x - Marlon Petit
-** Last update Thu Jan 14 12:47:07 2016 Marlon Petit
+** Last update Thu Jan 14 15:29:04 2016 Marlon Petit
 */
 
 #ifndef CHARACTER_HH_
 # define CHARACTER_HH_
 
+#include <iostream>
+#include <string>
+
 class	Character
 {
 public:
-  Character(const std::string &, int);
+  Character(const std::string &name, int level);
+  ~Character();
   const std::string	&getName() const;
   int			getLvl() const;
   int			getPv() const;
   int			getPower() const;
   int			CloseAttack();
+  int			RangeAttack();
+  void			Heal();
+  void		        RestorePower();
+  void			TakeDamage(int _damage);
+  enum			AttackRange{CLOSE, RANGE};
+  AttackRange		Range;
 private:
   const std::string	_name;
   int			_level;
